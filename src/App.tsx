410ce363b18b4f4e2cac651a23b26cd5758d11fe
Honzoraptor <j.ackermann@email.cz>
2023-12-23 14:58:18 +0100

    Fixed some css bugs on the home page.

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,18 @@ function App() {
 
 
                                                                                                                             
-    88        88                                                                                                            
-    88        88                                                                               ,d                           
-    88        88                                                                               88                           
-    88aaaaaaaa88   adPPYba   8IdPPYb  888888888  ,adPPYba,  8b,dPPYba, ,adPPYYba, 8b,dPPYba, MM88MMM ,adPPYba,  8b,dPPYba,  
-    88zzzzzzzz88 a8       8a 88P   8a     a8P   a8       8a 88P     Y8         Y8 88P'    "8a  88   a8"      8a 88P     Y8  
-    88        88 8b       d8 88    88   ,d8P    8b       d8 88         ,adPPPPP88 88       d8  88   8b       d8 88          
-    88        88  8a     a8  88    88 ,d8"      "8a,   ,a8" 88         88,    ,88 88b,   ,a8"  88,  "8a,   ,a8  88          
-    88        88    YbbdP    88    88 888888888   "YbbdP"'  88          "8bbdP"Y8 88 YbbdP"'    Y888   YbbdP    88          
-                                                                                                                            
+  88        88                                                                                                            
+  88        88                                                                               ,d                           
+  88        88                                                                               88                           
+  88aaaaaaaa88   adPPYba   8IdPPYb  888888888  ,adPPYba,  8b,dPPYba, ,adPPYYba, 8b,dPPYba, MM88MMM ,adPPYba,  8b,dPPYba,  
+  88zzzzzzzz88 a8       8a 88P   8a     a8P   a8       8a 88P     Y8         Y8 88P'    "8a  88   a8"      8a 88P     Y8  
+  88        88 8b       d8 88    88   ,d8P    8b       d8 88         ,adPPPPP88 88       d8  88   8b       d8 88          
+  88        88  8a     a8  88    88 ,d8"      "8a,   ,a8" 88         88,    ,88 88b,   ,a8"  88,  "8a,   ,a8  88          
+  88        88    YbbdP    88    88 888888888   "YbbdP"'  88          "8bbdP"Y8 88 YbbdP"'    Y888   YbbdP    88          
+                                                                                88
+                                                                                88                                        
 
-`, '-webkit-background-clip:text;background-image:-webkit-linear-gradient(40deg,#5C6AFF, #00FFFF);-webkit-text-fill-color:transparent;font-weight:000')
+`, "-webkit-background-clip:text;background-image:-webkit-linear-gradient(40deg,#5C6AFF, #00FFFF);-webkit-text-fill-color:transparent;font-weight:000")
   }
   return (
     <>
