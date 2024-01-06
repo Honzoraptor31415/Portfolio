4b7b4ea7002a0589d7b74ef980f6bf097dae1b46
Honzoraptor <j.ackermann@email.cz>
2024-01-06 18:13:49 +0100

    Trying VITE instead of REACT

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(import.meta.env.REACT_APP_SUPABASE_APP_URL, import.meta.env.REACT_APP_SUPABASE_APP_ANON_KEY)
+export const supabase = createClient(import.meta.env.VITE_APP_SUPABASE_APP_URL, import.meta.env.VITE_APP_SUPABASE_APP_ANON_KEY)
