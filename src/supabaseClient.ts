752205e30f6b571bed5fcbf302e68e2da531607d
Honzoraptor <j.ackermann@email.cz>
2024-01-06 18:04:47 +0100

    Fixing errors (not gonna work sadly )

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient("https://wkoaclnrbrydhqtsnisf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indrb2FjbG5yYnJ5ZGhxdHNuaXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQyMTA5MzIsImV4cCI6MjAxOTc4NjkzMn0.Zw-JOVRAcpHRlV2W7ArBbPM9N_VdxHXYcyBIVTnwaKc")
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_APP_URL!, process.env.REACT_APP_SUPABASE_APP_ANON_KEY!)
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+    REACT_APP_SUPABASE_APP_URL: process.env.REACT_APP_SUPABASE_APP_URL,
+    REACT_APP_SUPABASE_APP_ANON_KEY: process.env.REACT_APP_SUPABASE_APP_ANON_KEY
+  },
 })
