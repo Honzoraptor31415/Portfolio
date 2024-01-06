47e599f97d44da567f9169f94bd18770d2dece56
Honzoraptor <j.ackermann@email.cz>
2024-01-06 17:58:45 +0100

    Debugging g or gg or idk man

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-const appUrl = process.env.REACT_APP_SUPABASE_APP_URL
-const key = process.env.REACT_APP_SUPABASE_APP_ANON_KEY
-
-export const supabase = createClient(appUrl!, key!)
+export const supabase = createClient("https://wkoaclnrbrydhqtsnisf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indrb2FjbG5yYnJ5ZGhxdHNuaXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQyMTA5MzIsImV4cCI6MjAxOTc4NjkzMn0.Zw-JOVRAcpHRlV2W7ArBbPM9N_VdxHXYcyBIVTnwaKc")
