b875edb6a4163558daad4293610f11694062f4ca
Honzoraptor <j.ackermann@email.cz>
2024-01-06 18:09:38 +0100

    Trying something again

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-    REACT_APP_SUPABASE_APP_URL: process.env.REACT_APP_SUPABASE_APP_URL,
-    REACT_APP_SUPABASE_APP_ANON_KEY: process.env.REACT_APP_SUPABASE_APP_ANON_KEY,
-    REACT_APP_TEST_ENV: process.env.REACT_APP_TEST_ENV
-  },
 })
