5eaa156f4fcdda2790b91ae2c07a95f88151a255
Honzoraptor <j.ackermann@email.cz>
2024-01-06 19:33:44 +0100

    Cooking

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(import.meta.env.VITE_APP_SUPABASE_APP_URL, import.meta.env.VITE_APP_SUPABASE_APP_ANON_KEY)
+
+
+// if you're seeing this comment, my cooking didn't work
