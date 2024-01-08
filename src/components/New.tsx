11107fdcea6828d150725d5be04a5c4b2aec5c41
Honzoraptor <j.ackermann@email.cz>
2024-01-08 19:19:28 +0100

    Added languages for the /new page

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -81,57 +81,57 @@ function New() {
           <h3>{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Nový projekt" : "New project"}</h3>
           <div className="titles">
             <div className="title">
-              <label htmlFor="title">Titulek</label>
-              <input required type="text" placeholder="Title" id="title" onChange={handleCZTitleChange} value={CZtitle} />
+              <label htmlFor="title">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Titulek (CZ)" : "Title (CZ)"}</label>
+              <input required type="text" placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Titulek (CZ)" : "Title (CZ)"} id="title" onChange={handleCZTitleChange} value={CZtitle} />
             </div>
             <div className="title">
-              <label htmlFor="title">Title</label>
-              <input required type="text" placeholder="Title" id="title" onChange={handleENTitleChange} value={ENTitle} />
+              <label htmlFor="title">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Titulek (EN)" : "Title (EN)"}</label>
+              <input required type="text" placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Titulek (EN)" : "Title (EN)"} id="title" onChange={handleENTitleChange} value={ENTitle} />
             </div>
           </div>
 
           <div className="title">
-            <label htmlFor="date">Date</label>
-            <input type="text" id="date" placeholder="Date" onChange={handleDateChange} value={date} />
+            <label htmlFor="date">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Datum" : "Date"}</label>
+            <input type="text" id="date" placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Datum" : "Date"} onChange={handleDateChange} value={date} />
           </div>
 
           <div className="links">
             <div className="title">
-              <label htmlFor="img-url">Image URL</label>
-              <input placeholder="Image URL" required type="url" id="img-url" onChange={handleImgUrlChange} value={imgUrl} />
+              <label htmlFor="img-url">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "URL obrázku" : "Image URL"}</label>
+              <input placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "URL obrázku" : "Image URL"} required type="url" id="img-url" onChange={handleImgUrlChange} value={imgUrl} />
             </div>
 
             <div className="title">
-              <label htmlFor="github">Github link</label>
-              <input required placeholder="Github repo URL" type="url" id="github" onChange={handleGithubLinkChange} value={githubLink} />
+              <label htmlFor="github">{navigator.language === "cs" || "cs-CZ" ? "Odkaz na Github" : "Github link"}</label>
+              <input required placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Odkaz na Github repo" : "Github repo URL"} type="url" id="github" onChange={handleGithubLinkChange} value={githubLink} />
             </div>
 
             <div className="title">
-              <label htmlFor="url">Deployment link</label>
-              <input placeholder="Deployment link" type="url" id="url" onChange={handleUrlChange} value={url} />
+              <label htmlFor="url">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Odkaz na web" : "Deployment link"}</label>
+              <input placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Odkaz na web" : "Deployment link"} type="url" id="url" onChange={handleUrlChange} value={url} />
             </div>
 
             <div className="title">
-              <label htmlFor="download">Download link</label>
-              <input placeholder="Download link" type="text" id="download" onChange={handleDownloadChange} value={download} />
+              <label htmlFor="download">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Odkaz na stažení" : "Download link"}</label>
+              <input placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Odkaz na stažení" : "Download link"} type="text" id="download" onChange={handleDownloadChange} value={download} />
             </div>
           </div>
 
           <div className="text-title">
-            <label htmlFor="text">Text (CZ)</label>
-            <textarea placeholder="CZ text" required id="text" onChange={handleCZTextChange} value={CZText} ></textarea>
+            <label htmlFor="text">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Text (CZ)" : "Text (CZ)"}</label>
+            <textarea placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Text (CZ)" : "Text (CZ)"} required id="text" onChange={handleCZTextChange} value={CZText} ></textarea>
           </div>
 
           <div className="text-title">
-            <label htmlFor="text">Text (EN)</label>
-            <textarea placeholder="EN Text" required id="text" onChange={handleENTextChange} value={ENText} ></textarea>
+            <label htmlFor="text">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Text (EN)" : "Text (EN)"}</label>
+            <textarea placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Text (EN)" : "Text (EN)"} required id="text" onChange={handleENTextChange} value={ENText} ></textarea>
           </div>
 
-          <label htmlFor="tags">Tags</label>
-          <input placeholder="Tags" required type="text" id="tags" onChange={handleTagsChange} value={tags} />
+          <label htmlFor="tags">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Tagy" : "Tags"}</label>
+          <input placeholder={navigator.language === "cs" || navigator.language === "cs-CZ" ? "Tagy" : "Tags"} required type="text" id="tags" onChange={handleTagsChange} value={tags} />
 
           <div className="submit-wrp">
-            <button onClick={handleSubmit} id="submit">Submit</button>
+            <button onClick={handleSubmit} id="submit">{navigator.language === "cs" || navigator.language === "cs-CZ" ? "Odeslat" : "Submit"}</button>
           </div>
         </div>
       </header>
