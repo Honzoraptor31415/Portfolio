bb8003ef1726d5f28346a541f7a0394b6808a607
Honzoraptor <j.ackermann@email.cz>
2024-01-12 09:39:14 +0100

    Added and fixed mobile design
    Added mobile design for the /new page and fixed the overflow in mobile devices, which was caused by the cool background.

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -73,7 +73,7 @@ function New() {
   return (
     <>
       <header className="new">
-        <div style={{ position: "fixed", left: "50%", top: "50%", transform: "translate(-50%, -50%)", zIndex: "10" }}>
+        <div style={{ position: "fixed", left: "50%", top: "50%", transform: "translate(-50%, -50%)", zIndex: "10", width: "200px", height: "200px" }}>
           <BlurryBackground />
         </div>
         <div className="post-form">
