250ae17596e584b6973b84ee6069ee7ddc6f382e
Honzoraptor <j.ackermann@email.cz>
2024-04-27 16:33:23 +0200

    Added error handling for /projects
    I have no idea why I haven't done this earlier, but since my supabase portfolio project is currentely facing some issues, I had to quickly do this.

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -5,6 +5,8 @@ function Projects() {
   const [data, setData] = useState<any[]>([])
   const [copied, setCopied] = useState(false)
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  const [error, setError] = useState<string>()
+
   useEffect(() => {
     if (navigator.language === "cs-CZ" || navigator.language === "cs") {
       document.title = "Projekty"
@@ -29,15 +31,20 @@ function Projects() {
     /*  Date formating  */
     // res.data[0].date.split(".") = ex. ["10", "12", " 2023"]
     // res.data[0].date.split(".")[2].slice(1) = ex "2023" (cleans the whitespace)
-    res.data.sort((a: any, b: any) => {
-      try {
-        return new Date(`${b.date.split(".")[0]} ${months[b.date.split(".")[1] - 1]}, ${b.date.split(".")[2].slice(1)}`).valueOf() - new Date(`${a.date.split(".")[0]} ${months[a.date.split(".")[1] - 1]}, ${a.date.split(".")[2].slice(1)}`).valueOf()
-      } catch (error) {
-        console.error('An error occured while sorting the projects:', error)
-        return 0
-      }
-    })
-    setData(res.data)
+    if (res.data) {
+      res.data.sort((a: any, b: any) => {
+        try {
+          return new Date(`${b.date.split(".")[0]} ${months[b.date.split(".")[1] - 1]}, ${b.date.split(".")[2].slice(1)}`).valueOf() - new Date(`${a.date.split(".")[0]} ${months[a.date.split(".")[1] - 1]}, ${a.date.split(".")[2].slice(1)}`).valueOf()
+        } catch (error) {
+          console.error('An error occured while sorting the projects:', error)
+          return 0
+        }
+      })
+      setData(res.data)
+    } else {
+      setError("No projects")
+    }
+    res.error && setError("Something went wrong when fetching projects. I'm currentely investigating this bug.")
   }
 
   useEffect(() => {
@@ -54,105 +61,112 @@ function Projects() {
   return (
     <>
       <main className="projects">
-        {/* <div className="no-projects-wrp">
-          <div className="no-projects">
-            <h1>Currentely no projects</h1>
-            <p>Working on them.</p>
-          </div>
-        </div> */}
-        <h1>{navigator.language === "cs-CZ" || navigator.language === "cs" ? "Moje projekty" : "My projects"}</h1>
-        <div className="projects-wrp">
-          {data.map((value, index) => {
-            return (
-              <div onLoad={() => {
-                if (location.hash !== "") {
-                  document.getElementById(location.hash.slice(1))?.scrollIntoView()
-                }
-              }} key={index} className={`project ${location.hash.slice(1) === value.titleEn.replaceAll(" ", "-") ? "project-target" : ""}`} id={value.titleEn.replaceAll(" ", "-")}>
-                <div className="project-side">
-                  <a target="_blank" href={value.github}>
-                    <img src={value.img} className="rounded project-image" />
-                  </a>
-                </div>
-                <div className="project-side">
-                  <div className="project-top">
-                    <h3>{navigator.language === "cs" || navigator.language === "cs-CZ" ? value.titleCz : value.titleEn}</h3>
-                    {/* value.date.split(".") = ex. ["10", "12", " 2023"] 
+        {!error && data ? (
+          <>
+            <h1>{navigator.language === "cs-CZ" || navigator.language === "cs" ? "Moje projekty" : "My projects"}</h1>
+            <div className="projects-wrp">
+              {data.map((value, index) => {
+                return (
+                  <div onLoad={() => {
+                    if (location.hash !== "") {
+                      document.getElementById(location.hash.slice(1))?.scrollIntoView()
+                    }
+                  }} key={index} className={`project ${location.hash.slice(1) === value.titleEn.replaceAll(" ", "-") ? "project-target" : ""}`} id={value.titleEn.replaceAll(" ", "-")}>
+                    <div className="project-side">
+                      <a target="_blank" href={value.github}>
+                        <img src={value.img} className="rounded project-image" />
+                      </a>
+                    </div>
+                    <div className="project-side">
+                      <div className="project-top">
+                        <h3>{navigator.language === "cs" || navigator.language === "cs-CZ" ? value.titleCz : value.titleEn}</h3>
+                        {/* value.date.split(".") = ex. ["10", "12", " 2023"] 
 
                     value.date.split(".")[2].slice(1) = ex "2023" (cleans the whitespace for the year) */}
-                    <p className="date">{value.date.split(".")[0].length < 2 ? `0${value.date.split(".")[0]}` : value.date.split(".")[0]}.{value.date.split(".")[1].length < 2 ? `0${value.date.split(".")[1]}` : value.date.split(".")[1]}. {value.date.split(".")[2].slice(1)}</p>
-                  </div>
-                  {navigator.language === "cs-CZ" || navigator.language === "cs" ? (
-                    <p>{value.textCz}</p>
-                  ) : (
-                    <p>{value.textEn}</p>
-                  )}
-                  <div className="project-links">
-                    {value.web === "no" ? (
-                      <a target="_blank" href={value.github} className="before-bg-anim">
-                        <img src="/github-icon.svg" className="no-select" />
-                      </a>
-                    ) : ("")}
+                        <p className="date">{value.date.split(".")[0].length < 2 ? `0${value.date.split(".")[0]}` : value.date.split(".")[0]}.{value.date.split(".")[1].length < 2 ? `0${value.date.split(".")[1]}` : value.date.split(".")[1]}. {value.date.split(".")[2].slice(1)}</p>
+                      </div>
+                      {navigator.language === "cs-CZ" || navigator.language === "cs" ? (
+                        <p>{value.textCz}</p>
+                      ) : (
+                        <p>{value.textEn}</p>
+                      )}
+                      <div className="project-links">
+                        {value.web === "no" ? (
+                          <a target="_blank" href={value.github} className="before-bg-anim">
+                            <img src="/github-icon.svg" className="no-select" />
+                          </a>
+                        ) : ("")}
 
-                    {value.web === "yes" && value.link !== "no xD" ? (
-                      <>
-                        <a target="_blank" href={value.github} className="before-bg-anim">
-                          <img src="/github-icon.svg" className="no-select" />
-                        </a>
-                        <a target="_blank" href={value.link} className="before-bg-anim">
-                          <img src="/link-away.svg" className="no-select" />
-                        </a>
-                      </>
-                    ) : (
-                      <>
-                        {value.web === "yes" ? (
+                        {value.web === "yes" && value.link !== "no xD" ? (
                           <>
                             <a target="_blank" href={value.github} className="before-bg-anim">
                               <img src="/github-icon.svg" className="no-select" />
                             </a>
-                            <p className="no-demo">{navigator.language === "cs-CZ" || navigator.language === "cs" ? "Aplikace ještě není zveřejněná" : "App is not deployed yet"}</p>
+                            <a target="_blank" href={value.link} className="before-bg-anim">
+                              <img src="/link-away.svg" className="no-select" />
+                            </a>
                           </>
-                        ) : ("")}
-                      </>
-                    )}
+                        ) : (
+                          <>
+                            {value.web === "yes" ? (
+                              <>
+                                <a target="_blank" href={value.github} className="before-bg-anim">
+                                  <img src="/github-icon.svg" className="no-select" />
+                                </a>
+                                <p className="no-demo">{navigator.language === "cs-CZ" || navigator.language === "cs" ? "Aplikace ještě není zveřejněná" : "App is not deployed yet"}</p>
+                              </>
+                            ) : ("")}
+                          </>
+                        )}
 
-                  </div>
-                  <div className="project-bottom-wrp">
-                    <div className="tags-wrp">
-                      <p>{navigator.language === "cs-CZ" || navigator.language === "cs" ? "Tagy:" : "Tags:"}</p>
-                      <div className="tags">
-                        {value.tags.split(" ").map((v: any, i: any) => {
-                          return (
-                            <p key={i} className={`tag ${v}-tag`} >{v}</p>
-                          )
-                        })}
+                      </div>
+                      <div className="project-bottom-wrp">
+                        <div className="tags-wrp">
+                          <p>{navigator.language === "cs-CZ" || navigator.language === "cs" ? "Tagy:" : "Tags:"}</p>
+                          <div className="tags">
+                            {value.tags.split(" ").map((v: any, i: any) => {
+                              return (
+                                <p key={i} className={`tag ${v}-tag`} >{v}</p>
+                              )
+                            })}
+                          </div>
+                        </div>
+                        <div className="popover-wrp">
+                          <button id={`btn-${index}`} className="three-dots-btn popover-btn before-bg-anim">
+                            <img className="three-dots-vertical" src="/three-dots-vertical-icon.svg" alt="" />
+                            <div id={`popover-${index}`} className="popover">
+                              <a onClick={() => {
+                                setCopied(true)
+                                setTimeout(() => {
+                                  setCopied(false)
+                                  document.getElementById(`btn-${index}`)?.blur()
+                                }, 1000)
+                                clipboardWrite(`https://honzoraptor.vercel.app/projects#${value.titleEn.replaceAll(" ", "-")}`)
+                              }} className="popover-link">
+                                <span className={copied ? "success-text" : ""}>
+                                  {copied ? navigator.language === "cs" || navigator.language === "cs-CZ" ? "Zkopírováno!" : "Link copied!" : navigator.language === "cs" || navigator.language === "cs-CZ" ? "Kopírovat odkaz" : "Copy link"}
+                                </span>
+                              </a>
+                            </div>
+                          </button>
+                        </div>
                       </div>
                     </div>
-                    <div className="popover-wrp">
-                      <button id={`btn-${index}`} className="three-dots-btn popover-btn before-bg-anim">
-                        <img className="three-dots-vertical" src="/three-dots-vertical-icon.svg" alt="" />
-                        <div id={`popover-${index}`} className="popover">
-                          <a onClick={() => {
-                            setCopied(true)
-                            setTimeout(() => {
-                              setCopied(false)
-                              document.getElementById(`btn-${index}`)?.blur()
-                            }, 1000)
-                            clipboardWrite(`https://honzoraptor.vercel.app/projects#${value.titleEn.replaceAll(" ", "-")}`)
-                          }} className="popover-link">
-                            <span className={copied ? "success-text" : ""}>
-                              {copied ? navigator.language === "cs" || navigator.language === "cs-CZ" ? "Zkopírováno!" : "Link copied!" : navigator.language === "cs" || navigator.language === "cs-CZ" ? "Kopírovat odkaz" : "Copy link"}
-                            </span>
-                          </a>
-                        </div>
-                      </button>
-                    </div>
                   </div>
-                </div>
+                )
+              })}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="no-projects-wrp">
+              <div className="no-projects">
+                <h1>Error</h1>
+                <p>{error}</p>
               </div>
-            )
-          })}
-        </div>
+            </div>
+          </>
+        )}
       </main>
     </>
   )
